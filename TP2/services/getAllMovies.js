fe19405b63e7e03c5facc2caa86ea20376558d83
Mateo Garcia <mateogarcia133@hotmail.com>
2026-04-21 16:49:21 -0300

    estrucutrando

--- TP2/services/getAllMovies.js
+++ TP2/services/getAllMovies.js
@@ -3,19 +3,16 @@ const API_URL = "https://69e65c86ce4e908a155f6c79.mockapi.io/api/v1/pelicula";
 export const getAllMovies = async (page = 1, limit = 8, search = "") => {
   try {
     const url = new URL(API_URL);
-
     url.searchParams.append("page", page.toString());
     url.searchParams.append("limit", limit.toString());
-
     if (search) {
       url.searchParams.append("Title", search);
     }
-
     const response = await fetch(url);
     if (!response.ok) throw new Error("Error en la red");
-
     const data = await response.json();
-    return data;
+    const finalData = Array.isArray(data[0]) ? data[0] : data;
+    return Array.isArray(finalData) ? finalData : [];
   } catch (error) {
     console.error("Error en getAllMovies:", error);
     return [];
--- TP2/src/pages/Home/Home.jsx
+++ TP2/src/pages/Home/Home.jsx
@@ -3,18 +3,52 @@ import styles from "./Home.module.css";
 import { getAllMovies } from "../../../services/getAllMovies";
 
 const Home = () => {
-  const [movies, setMovies] = useState([]);
-  const [page, setPage] = useState(1);
-  const [search, setSearch] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [todasLasPelis, setTodasLasPelis] = useState([]);
+  const [peliculasVisibles, setPeliculasVisibles] = useState([]);
+  const [pagina, setPagina] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const limitePorPagina = 8;
   useEffect(() => {
-    const loadData = async () => {
-      const data = await getAllMovies(page, 8, search);
-      setMovies(data);
+    const cargarTodo = async () => {
+      setLoading(true);
+      const respuesta = await getAllMovies(1, 100);
+      setTodasLasPelis(respuesta);
+      setPeliculasVisibles(respuesta.slice(0, limitePorPagina));
+      setLoading(false);
     };
-    loadData();
-  }, [page, search]);
+    cargarTodo();
+  }, []);
 
-  return <h1>Películas encontradas: {movies.length}</h1>;
+  const cargarMas = () => {
+    const nuevaPagina = pagina + 1;
+    const inicio = 0;
+    const fin = nuevaPagina * limitePorPagina;
+
+    setPeliculasVisibles(todasLasPelis.slice(inicio, fin));
+    setPagina(nuevaPagina);
+  };
+
+  return (
+    <main>
+      <h1>Cartelera</h1>
+
+      {loading && <p>Cargando catálogo...</p>}
+
+      <section className={styles.grid}>
+        {peliculasVisibles.map((item) => (
+          <article key={item.imdbID}>
+            <h2>{item.Title}</h2>
+          </article>
+        ))}
+      </section>
+
+      {peliculasVisibles.length < todasLasPelis.length && (
+        <button onClick={cargarMas} className={styles.btnCargar}>
+          Cargar más
+        </button>
+      )}
+    </main>
+  );
 };
+
 export default Home;
