888b3bcc97454cda36d9a2e3c651904d3ea4205b
Mateo Garcia <mateogarcia133@hotmail.com>
2026-05-04 15:38:59 -0300

    Funcionalidad pagina detalles y cambio a omdbAPI

--- TP2/services/getAllMovies.js
+++ TP2/services/getAllMovies.js
@@ -1,12 +1,10 @@
-const API_KEY = "4a68574c"; // La que te llegó al mail
+const API_KEY = "4a68574c";
 const BASE_URL = "https://www.omdbapi.com/";
 
 export const getAllMovies = async (page = 1, search = "Batman") => {
   try {
     const url = new URL(BASE_URL);
     url.searchParams.append("apikey", API_KEY);
-
-    // OMDb usa 's' para buscar y 'page' para paginar
     url.searchParams.append("s", search);
     url.searchParams.append("page", page.toString());
 
@@ -14,10 +12,7 @@ export const getAllMovies = async (page = 1, search = "Batman") => {
     if (!response.ok) throw new Error("Error en la red");
 
     const data = await response.json();
-
-    // OMDb devuelve { Search: [...], totalResults: "123", Response: "True" }
     if (data.Response === "True") {
-      // Retornamos solo el array de películas
       return data.Search;
     }
 
