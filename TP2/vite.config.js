96cf1925c6259c1835d5265d76ba93b7dcda1204
Mateo Garcia <mateogarcia133@hotmail.com>
2026-05-13 16:10:14 -0300

    configuracion inicial

--- TP2/vite.config.js
+++ TP2/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [react()],
+  test: {
+    // Habilita las APIs globales de Vitest (como Jest)
+    globals: true,
+    // Configura jsdom como entorno de testing
+    environment: 'jsdom',
+    // Indica el archivo de configuración inicial
+    setupFiles: './src/setupTests.js',
+  },
 });
